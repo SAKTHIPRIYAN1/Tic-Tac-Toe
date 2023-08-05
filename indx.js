5fd21cb0963f98282936889ae362af775962b169
“birthdayceleb” <sakthipriyan2935@gmail.com>
2023-08-05 14:59:56 +0530

    finished double player mode
    finished but next time not working....

--- indx.js
+++ indx.js
@@ -80,8 +80,16 @@ function clo(){
 let s=document.querySelectorAll(".s")
 let mode_all=['player','computer'];
 function choose(n){
-    s[n].classList.add("none")
-    s[n+1].classList.remove("none")
+   
+    
+    for(let i=0;i<s.length;i++){
+        if(n==i){
+            s[n].classList.remove("none")
+        }
+        else{
+            s[i].classList.add("none")
+        }
+    }
 }
 let against=null;
 function mode(n){
@@ -107,14 +115,34 @@ function mode(n){
 //  check draw...
 
 
+//// the winning combinationsss.....
+
+const win_com=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
 const x_cls='cll_x'
 const cir_cls='cll_cir'
 let circle_cls=true;
 
+
 // function when the player chooses that he wants to play against a player...../
 let cellelements=document.querySelectorAll(".cll")
+let brd=document.querySelector(".brd")
+let temp_str=brd.innerHTML;
 function playergame(){
     console.log('chooosed player game....')
+
+    /// hovre effect initailly....
+     hoverefft(circle_cls)
+
      /// to make the cell  clicked once...
     cellelements.forEach(cell=>{
         cell.addEventListener('click',clk_event,{once:true})
@@ -126,25 +154,79 @@ function playergame(){
 
         // diplay x or O while clicking......
         display_(cell, current_cls)
+
         /// to swappp the turns after clicking one cellll.....
         swap()
 
+        ///check win....
+       let win= check(current_cls)
+       if(win){
+        console.log('matched...')
+        endgame(current_cls)
+       }
+
+        /// hover effect after swap...
+        hoverefft(circle_cls)
     }
 
     function display_(cell,class_){
         cell.classList.add(class_)
         console.log("a event occured...")
     }
+
+    /// checking before winnig the game.......
+    function check(class_){
+       return win_com.some(comb=>{
+            return comb.every(index=>{
+                return cellelements[index].classList.contains(class_)
+            }
+            )
+        })
+    }
+
+    ///ending the game....
+  
+
+
     function swap(){
         circle_cls=!circle_cls;
         console.log("swapped"+circle_cls)
     }
 
+    function hoverefft(tf){
+        brd.classList.remove("brd_x")
+        brd.classList.remove("brd_cir")
+
+        if(tf){
+            brd.classList.add("brd_cir")
+        }
+        else{
+            brd.classList.add("brd_x")
+        }
+    }
+
+}
+let win_div=document.querySelector('.win_msg')
+function endgame(class_){
+    let winner;
+  if(class_=='cll_cir'){
+    winner='O';
+  }
+  else{
+    winner='X';
+  }
+   win_div.innerHTML=`
+                <i class="fa-solid fa-circle-xmark" onclick="clo()" id="cl"></i>
+                <h2 class='det_h'>${winner} wins the game</h2>
+   `;
+   start()
+   choose(2)
+    console.log("game ended.........");
+    brd.innerHTML=temp_str
+    playergame()
 }
 
-
 // function when the player wants to play against the computer....
-
 function computergame(){
     console.log("computer game")
 }
